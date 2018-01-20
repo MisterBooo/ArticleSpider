ed1b53d0f22e3452eb98f84ee8b0f5b81e3667d9
Misterbigbooo <Zeno@ZenodeMacBook-Pro.local>
2018-01-20 21:25:19 +0800

    xpath学习

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -9,5 +9,10 @@ class JobboleSpider(scrapy.Spider):
 
     def parse(self, response):
         # // *[ @ id = "post-110287"] / div[1] / h1
-        response_selector = response.xpath("/html/body/div[3]/div[3]/div[1]/div[1]/h1")
+        re2_selector = response.xpath('//*[@id="post-110287"]/div[1]/h1/text()')
+        re3_selector = response.xpath("/html/body/div[1]/div[3]/div[1]/div[1]/h1/text()")
+        re_selector = response.xpath("/html/body/div[3]/div[3]/div[1]/div[1]/h1")
+        # 推荐使用class型，因为后期循环爬取可扩展通用性强。
+        re4_selector = response.xpath('//div[@class="entry-header"]/h1/text()')
+
         pass
