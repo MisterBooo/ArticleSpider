e3d32e20e0cc3272aaa04c7990c4e7a3a2733df7
Misterbigbooo <Zeno@ZenodeMacBook-Pro.local>
2018-01-21 10:09:26 +0800

    文章数据提取

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import re
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
@@ -8,11 +8,26 @@ class JobboleSpider(scrapy.Spider):
     start_urls = ['http://blog.jobbole.com/110287/']
 
     def parse(self, response):
-        # // *[ @ id = "post-110287"] / div[1] / h1
-        re2_selector = response.xpath('//*[@id="post-110287"]/div[1]/h1/text()')
-        re3_selector = response.xpath("/html/body/div[1]/div[3]/div[1]/div[1]/h1/text()")
-        re_selector = response.xpath("/html/body/div[3]/div[3]/div[1]/div[1]/h1")
+
         # 推荐使用class型，因为后期循环爬取可扩展通用性强。
-        re4_selector = response.xpath('//div[@class="entry-header"]/h1/text()')
+        title = response.xpath('//div[@class="entry-header"]/h1/text()').extract_first("")
+        create_date = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()').extract()[0].strip().replace("·",'').strip()
+        praise_nums = response.xpath('//span[contains(@class,"vote-post-up")]/h10/text()').extract_first("")
+        fav_nums =  response.xpath('//span[contains(@class,"bookmark-btn")]/text()').extract_first("")
+        match_re = re.match(".*?(\d+).*?",fav_nums)
+        if match_re:
+            fav_nums = match_re.group(1)
+
+        comment_nums = response.xpath("//a[@href='#article-comment']/span/text()").extract()[0]
+        match_re = re.match(".*?(\d+).*", comment_nums)
+        if match_re:
+            comment_nums = match_re.group(1)
+
+        content = response.xpath("//div[@class='entry']").extract()[0]
+
+        tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+        tags = ",".join(tag_list)
+
 
         pass
